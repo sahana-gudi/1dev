99c97a3c72be1be4aec3809c80ff0eb645d58468
sahana-gudi <sonugudi472@gmail.com>
2026-01-16 18:19:19 +0530

    saving

---
+++ temp(1).py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv)!=2:
+    print("Usage:python temp(1).php<temp_in_celsius>")
+    sys.exit(1)
+try:
+    celsius=float(sys.argv[1])
+    fahrenheit=(celsius*9/5)+32
+    print(f"{celsius}°C is equal to {fahrenheit}°F")
+
+except ValueError:
+    print ("Plese Enter a valid numeric value for celsius temp")
